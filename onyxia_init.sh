47bb6da4b9b32a59f8da7b964c220c50628e0199
tgiovansili <tgiovansili@hotmail.fr>
2026-02-05 13:57:31 +0100

    new install

--- onyxia_init.sh
+++ onyxia_init.sh
@@ -1,13 +1,21 @@
-jupyter
-numpy
-pandas
-requests
-opencv-python
-scikit-image
-seaborn
-matplotlib
-tqdm
-annoy
-torch
-torchvision
-scikit-learn
+#!/bin/bash
+set -e
+
+echo "Installing Python dependencies..."
+
+pip install \
+  jupyter \
+  numpy \
+  pandas \
+  requests \
+  opencv-python \
+  scikit-image \
+  seaborn \
+  matplotlib \
+  tqdm \
+  annoy \
+  torch \
+  torchvision \
+  scikit-learn
+
+echo "Done."
